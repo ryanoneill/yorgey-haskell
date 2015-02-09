c5e7e3edb6e645b5c27a596ecac1bc8e76da7d1a
Ryan O'Neill <ryanoneill@gmail.com>
2015-02-08 21:04:48 -0800

    Added histogram

--- Week03/Golf.hs
+++ Week03/Golf.hs
@@ -1,6 +1,7 @@
 module Golf where
 
-import Data.List (tails, zip3)
+import Data.List (group, intercalate, sort, tails, transpose, zip3)
+import Data.Maybe (fromMaybe)
 
 skips :: [a] -> [[a]]
 skips = map skipper . init . tails . zip [1..]
@@ -18,3 +19,9 @@ localMaxima xs = map s $ filter p $ zip3 xs ys zs
     p  = \(x,y,z) -> y > x && y > z
     zs = tail ys
     ys = tail xs
+
+histogram xs = (intercalate "\n" $ transpose stars) ++ "\n==========\n0123456789\n"
+  where
+    stars = map (\i -> replicate (maximum counts - i) ' ' ++ replicate i '*') counts
+    counts = map (fromMaybe 0 . flip lookup db) [0..9]
+    db = map (\l -> (head l, length l)) $ group $ sort xs
